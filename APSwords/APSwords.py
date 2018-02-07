d3beebd3405b4d14cedbd4501adf949b409a1e6e
jlpienaar <34496265+jlpienaar@users.noreply.github.com>
2018-02-07 20:46:38 +0000

    Update APSwords.py

--- APSwords/APSwords.py
+++ APSwords/APSwords.py
@@ -3,10 +3,11 @@ import sys
 
 def main():
     #extract text as a string from file
-    file = open('test2.tex','r')
+    filename=input('Enter the name of the .tex file you want to count: ')
+    file = open(filename,'r')
     string = file.read()
     file.close()
-    #trim the string down to the main body (the part between abstract and ac knowledgements or bibliography, whichever comes first). This avoids capturing sections of text from eg. the appendix or elsewhere. Exclude the start and end tokens.
+    #trim the string to the main body (the part between abstract and ac knowledgements or bibliography, whichever comes first). This avoids capturing sections of text from eg. the appendix or elsewhere. Exclude the start and end tokens.
     match=re.search(r'(?:\\end\{abstract\}(.*?)\\begin\{(thebibliography|acknowledgments)\})', string, flags=re.DOTALL)    
     #exit with error if the result is empty
     if not match:
@@ -15,13 +16,16 @@ def main():
     #Enclose main body between a begin{...} and end{...} that the subsequent regex will recognize.
     trim='\end{figure}'+match.group(1)+'\\begin{figure}'   
     #in the main body, capture only text in between certain pre-identified delimiters, eg. from the ending of the abstract, figures, or equations to the beginning of the next figure, equation, acknowledgements or bibliography.
-    matchtuple=re.findall(r'(?:\\end\{(equation|figure)\}(.*?)\\begin\{(equation|figure)\})', trim, flags=re.DOTALL)
+    matchtuple=re.findall(r'(?:\\end\{(equation|figure|eqnarray)\}(.*?)\\begin\{(equation|figure|eqnarray)\})', trim, flags=re.DOTALL)
+    #Exit if there is no match
     if not matchtuple:
         print('no match')
         sys.exit(1)
+    #Combine the extracted blocks of text into a single string    
     words=''
     for i in range(0,len(matchtuple)):
         words+=matchtuple[i][1]
+    #count the words and add to the counts obtained for images and displayed math    
     wordlist=words.split()
     ar1=115/86
     ar2=277/61
